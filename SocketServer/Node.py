feb2c2d3f176be113eae999ecafd00e683ab0632
kostya <konstantin.uvar@gmail.com>
2020-03-17 10:10:27 +0300

    fix node status fix closed connection error

--- SocketServer/Node.py
+++ SocketServer/Node.py
@@ -1,12 +1,7 @@
 from threading import Thread
 from Messages import StartAttackMessage, StopAttackMessage
 
-STATUS = {
-    'offline': -1,
-    'online': 0,
-    'attacked': 1,
-    'down': 2
-}
+STATUS = ['offline', 'online', 'attacked', 'down']
 
 
 class ClientNode(Thread):
@@ -14,13 +9,13 @@ class ClientNode(Thread):
         super().__init__()
 
         self.address = None
-        self.status = STATUS['offline']
+        self.status = STATUS[0]
 
         self.__running = True
         self.connection = connection
 
     def run(self) -> None:
-        self.status = STATUS['online']
+        self.status = STATUS[1]
         while self.__running:
             data = self.connection.recv(2048).decode("UTF8")
             if len(data) and data[-1] == '\n':
@@ -30,24 +25,24 @@ class ClientNode(Thread):
         print("i: {}".format(data))
 
     def set_status(self, int_code):
-        if int_code in STATUS.items():
-            self.status = int_code
+        if 0 <= int_code <= len(STATUS):
+            self.status = STATUS[int_code]
 
     def set_address(self, address):
         self.address = address
 
     def stop(self):
         self.__running = False
-        self.status = STATUS['offline']
+        self.status = STATUS[0]
         self.connection.close()
 
     def start_attack(self):
         self.send_message(StartAttackMessage().get_message())
-        self.status = STATUS['attacked']
+        self.status = STATUS[2]
 
     def stop_attack(self):
         self.send_message(StopAttackMessage().get_message())
-        self.status = STATUS['online']
+        self.status = STATUS[1]
 
     def send_message(self, message):
         print("o: {}".format(message))
--- SocketServer/NodePool.py
+++ SocketServer/NodePool.py
@@ -4,17 +4,6 @@ class ClientNodePool:
         self.__iter_value = 0
         self.client_nodes = []
 
-    # def __iter__(self):
-    #     return self
-    #
-    # def __next__(self):
-    #     self.__iter_value += 1
-    #     if not len(self._client_nodes) \
-    #             or self.__iter_value > len(self._client_nodes):
-    #         raise StopIteration
-    #     else:
-    #         return self._client_nodes[self.__iter_value - 1]
-
     def add(self, client_node):
         self.client_nodes.append(client_node)
 
@@ -25,8 +14,14 @@ class ClientNodePool:
 
     def start_attack(self):
         for node in self.client_nodes:
-            node.start_attack()
+            try:
+                node.start_attack()
+            except (BrokenPipeError, OSError):
+                node.stop()
 
     def stop_attack(self):
         for node in self.client_nodes:
-            node.stop_attack()
+            try:
+                node.stop_attack()
+            except (BrokenPipeError, OSError):
+                node.stop()
